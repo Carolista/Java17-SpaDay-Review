b55751c956543cf2409f22ed08b10bd627a017b9
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-05 20:52:11 -0400

    completed bonus mission 1

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,12 +1,10 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.UserData;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -25,7 +23,15 @@ public class UserController {
             model.addAttribute("error", "Passwords do not match.");
             return "/user/add";
         }
-        model.addAttribute("username", user.getUsername());
+        UserData.add(user);
+        model.addAttribute("user", user);
+        model.addAttribute("users", UserData.getAll());
         return "/user/index";
+    }
+
+    @GetMapping("/details/{id}")
+    public String displayUserDetails(@PathVariable int id, Model model) {
+        model.addAttribute("user", UserData.getById(id));
+        return "/user/details";
     }
 }
